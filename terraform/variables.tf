e5d0a829de717b65c2d4e42545740dd0dc51870c
Eugenio Guarino <eugenio-97@live.it>
2024-03-24 21:44:21 +0100

    feat: modify variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,19 @@
 variable "region" {
-  type = string
+
 }
 
 variable "project_id" {
-  description = "The project_id ID"
-  type        = string
+
 }
 
 variable "service_account" {
-  description = "The service account to use for running the workflow and triggering the workflow by Cloud Scheduler - If empty or null a service account will be created. If you have provided a service account you need to grant the Cloud SQL Admin and the Workflows Invoker role to that"
-  type        = string
-  default     = null
+
 }
 
 variable "instance_name" {
-  type = string
+
 }
 
 variable "zone" {
-  type = string
+
 }
