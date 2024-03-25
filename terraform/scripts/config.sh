67e6e33e7470b19ed5925349f48d821a5c4ee6b4
Eugenio Guarino <eugenio-97@live.it>
2024-03-25 08:41:33 +0100

    feat: update timings

--- terraform/scripts/config.sh
+++ terraform/scripts/config.sh
@@ -32,8 +32,8 @@ mount -o discard,defaults /dev/sdb /mnt/disks/aria-data-disk
 # run minecraft docker image
 docker run --privileged -d -v /mnt/disks/aria-data-disk/:/data \
     -e TYPE=FORGE -e MEMORY=25G -e DEBUG=true \
-    -e ENABLE_AUTOSTOP=TRUE -e AUTOSTOP_TIMEOUT_EST=600 \
-    -e AUTOSTOP_TIMEOUT_INIT=800 \
+    -e ENABLE_AUTOSTOP=TRUE -e AUTOSTOP_TIMEOUT_EST=300 \
+    -e AUTOSTOP_TIMEOUT_INIT=500 \
     -e VERSION=1.19.2 -e FORGE_VERSION=43.2.0 \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server:java17
 
