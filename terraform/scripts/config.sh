aeb415e46c039b2f4b2cfd6b8ba8e349d0d066bc
Eugenio Guarino <eugenio-97@live.it>
2024-04-01 02:00:13 +0200

    feat: disable pvp VM

--- terraform/scripts/config.sh
+++ terraform/scripts/config.sh
@@ -31,7 +31,7 @@ mount -o discard,defaults /dev/sdb /mnt/disks/aria-data-disk
 
 # run minecraft docker image
 docker run --privileged -d -v /mnt/disks/aria-data-disk/:/data \
-    -e TYPE=FORGE -e MEMORY=28G -e DEBUG=true \
+    -e TYPE=FORGE -e MEMORY=28G -e DEBUG=true -e PVP=false \
     -e ENABLE_AUTOSTOP=TRUE -e AUTOSTOP_TIMEOUT_EST=300 \
     -e AUTOSTOP_TIMEOUT_INIT=500 -e ONLINE_MODE=false \
     -e VERSION=1.19.2 -e FORGE_VERSION=43.2.0 \
