01eeeabf4ebba8cf6b0bd2a1a8efb2600f404182
Eugenio Guarino <eugenio-97@live.it>
2024-04-08 19:42:54 +0200

    feat: modify timer for shutdown

--- terraform/scripts/config.sh
+++ terraform/scripts/config.sh
@@ -32,8 +32,8 @@ mount -o discard,defaults /dev/sdb /mnt/disks/aria-data-disk
 # run minecraft docker image
 docker run --privileged -d -v /mnt/disks/aria-data-disk/:/data \
     -e TYPE=FORGE -e MEMORY=28G -e DEBUG=true -e PVP=false \
-    -e ENABLE_AUTOSTOP=TRUE -e AUTOSTOP_TIMEOUT_EST=300 \
-    -e AUTOSTOP_TIMEOUT_INIT=500 -e ONLINE_MODE=false \
+    -e ENABLE_AUTOSTOP=TRUE -e AUTOSTOP_TIMEOUT_EST=350 \
+    -e AUTOSTOP_TIMEOUT_INIT=600 -e ONLINE_MODE=false \
     -e VERSION=1.19.2 -e FORGE_VERSION=43.2.0 \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server:java17
 
