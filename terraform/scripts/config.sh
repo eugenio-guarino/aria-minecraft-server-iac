42d8c286986b807b4263f2cde2fb9025a9ebf7a3
Eugenio Guarino <eugenio-97@live.it>
2024-04-11 00:57:28 +0200

    feat: repair startup script

--- terraform/scripts/config.sh
+++ terraform/scripts/config.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 sudo su
 
+# mount minecraft data disk
+mkdir -p /mnt/disks/aria-data-disk
+mount -o discard,defaults /dev/sdb /mnt/disks/aria-data-disk
+
+# update mods
+gsutil -m rsync -d gs://aria-minecraft-server/mods /mnt/disks/aria-data-disk/mods
+
+# create scripts for server
+mkdir /opt/scripts
+gsutil -m cp -r gs://aria-minecraft-server/scripts/* /opt/scripts/
+
 # upgrade
 apt update
 apt upgrade -y
@@ -21,18 +32,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-# create scripts for server
-mkdir /opt/scripts
-gsutil -m cp -r gs://aria-minecraft-server/scripts/* /opt/scripts/
-
-# mount minecraft data disk
-mkdir -p /mnt/disks/aria-data-disk
-mount -o discard,defaults /dev/sdb /mnt/disks/aria-data-disk
-
-# update mods
-gsutil -m rsync -r -d gs://aria-minecraft-server/mods /mnt/disks/aria-data-disk/mods
-
-
 # # run minecraft docker image
 docker run --privileged -d -v /mnt/disks/aria-data-disk/:/data \
     -e TYPE=FORGE -e MEMORY=28G -e DEBUG=true -e PVP=false \
@@ -41,12 +40,10 @@ docker run --privileged -d -v /mnt/disks/aria-data-disk/:/data \
     -e VERSION=1.19.2 -e FORGE_VERSION=43.2.0 \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server:java17
 
-
 sleep 90s
 
-# # send out ip address
+## send the ip address to the telegram group
 nohup bash /opt/scripts/send_ip_address.sh </dev/null &>/dev/null &
 
 # # autodestroy when CPU usage is low
 nohup bash /opt/scripts/auto_destroy.sh </dev/null &>/dev/null &
-
